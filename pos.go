fc6bfc2fbcf5109b6b7f7a16fde1f10414d908c9
rhykw <dev@rhykw.net>
2024-04-02 10:48:32 +0000

    convert many struct and separate pos integration

--- pos.go
+++ pos.go
@@ -12,26 +12,6 @@ type Pos [2]float32
 // PosList position list
 type PosList []Pos
 
-// Area strunct of area
-type Area struct {
-	PosList PosList `xml:"http://www.opengis.net/gml/3.2 Surface>patches>PolygonPatch>exterior>Ring>curveMember>Curve>segments>LineStringSegment>posList"`
-}
-
-// AdmArea struct of AdmArea
-type AdmArea struct {
-	OrgGILvl string `xml:"orgGILvl"`
-	Fid      string `xml:"fid"`
-	Vis      string `xml:"vis"`
-	Name     string `xml:"name"`
-	Code     string `xml:"admCode"`
-	Area     Area   `xml:"area"`
-}
-
-// Dataset struct of Dataset
-type Dataset struct {
-	Data []AdmArea `xml:"AdmArea"`
-}
-
 // UnmarshalXML UnmarshalXML for PosList
 func (pl *PosList) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	p := struct {
